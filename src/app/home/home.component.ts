a90a0afdabf43e0faec4387b5425fddaa246899d
gauravnegi101 <gauravnegi@csgroupchd.com>
2024-02-13 18:21:00 +0530

    update code

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [SlickCarouselModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
